352801aa9b414fd1d22df5b6449acc032d68a19b
qq1270735037 <1270735037@qq.com>
2023-04-08 20:24:05 +0800

    修理增加网格

--- pages/index2/index.vue
+++ pages/index2/index.vue
@@ -1,30 +1,47 @@
 <template>
 
 	<view>
-		<u-row >
-			<u-col span="2.8">
-				<view >
-					<text style="font-size: 35upx;margin-left: 15upx; font-weight:600" >个人信息:</text>
-				</view>
-			</u-col>
-			<u-col >
-				<view >
-					<text style="font-size: 35upx;margin-left: 15upx; font-weight:600">{{name}}</text>
-				</view>
-			</u-col>
-		</u-row>
-		<u-row >
-			<u-col span="3">
-				<view >
-					<text style="font-size: 35upx;margin-left: 15upx; font-weight:600">联系方式:</text>
-				</view>
-			</u-col>
-			<u-col span="9">
-				<view class="inputview">
-						<input class="form_style" type="number" v-model="phone" placeholder="请输入联系方式" ></input>
-				</view>
-			</u-col>
-		</u-row>
+		<view>
+			<u-row>
+				<u-col span="2.8">
+					<view>
+						<text style="font-size: 35upx;margin-left: 15upx; font-weight:600">个人信息:</text>
+					</view>
+				</u-col>
+				<u-col>
+					<view>
+						<text style="font-size: 35upx;margin-left: 15upx; font-weight:600">{{name}}</text>
+					</view>
+				</u-col>
+			</u-row>
+			<u-row>
+				<u-col span="3">
+					<view>
+						<text style="font-size: 35upx;margin-left: 15upx; font-weight:600">联系方式:</text>
+					</view>
+				</u-col>
+				<u-col span="9">
+					<view class="inputview">
+						<input class="form_style" type="number" v-model="phone" placeholder="请输入联系方式"></input>
+					</view>
+				</u-col>
+			</u-row>
+		</view>
+
+
+		<view class="grid">
+			<view>
+				<text style="font-size: 35upx;margin-left: 15upx; font-weight:600">报修物品:</text>
+			</view>
+			<uni-grid  :column="3" :highlight="true" @change="change">
+				<uni-grid-item v-for="(item, index) in 8" :index="index" :key="index">
+					<view class="grid-item-box" >
+						<uni-icons type="image" :size="40" color="#777" />
+						<text class="text">文本信息</text>
+					</view>
+				</uni-grid-item>
+			</uni-grid>
+		</view>
 
 		<tabBar :current="3"></tabBar>
 	</view>
@@ -34,18 +51,24 @@
 	export default {
 		data() {
 			return {
-				name:'',
+				name: '',
 				phone: '',
-					
-				
+
+
 
 			}
 
 		},
 		methods: {
-			sexSelect(e) {
-				this.model1.userInfo.sex = e.name
-				this.$refs.uForm.validateField('userInfo.sex')
+			change(e) {
+				let {
+					index
+				} = e.detail
+
+				uni.showToast({
+					title: `点击第${index+1}个宫格`,
+					icon: 'none'
+				})
 			},
 		}
 	}
@@ -70,7 +93,8 @@
 		width: 500upx;
 		border-radius: 20upx;
 	}
-	.inputview{
+
+	.inputview {
 		background-color: #e6e6e6;
 		font-size: 35upx;
 		height: 80upx;
@@ -78,4 +102,32 @@
 		margin-top: 6upx;
 		border-radius: 20upx;
 	}
+
+	.image {
+		width: 25px;
+		height: 25px;
+	}
+
+	.text {
+		font-size: 20px;
+		margin-top: 5px;
+	}
+
+
+	.grid{
+		padding-top: 20upx;
+	}
+
+	.grid-item-box {
+		flex: 1;
+		/* position: relative; */
+		/* #ifndef APP-NVUE */
+		display: flex;
+		/* #endif */
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		padding: 15px 0;
+	}
+	
 </style>
